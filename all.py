63ec6cd3928c9241d37c77efff20e0f661cdf9dc
Alexander <surfmetrics@gmail.com>
2023-08-28 13:23:34 +0200

    script to run everything

---
+++ all.py
@@ -0,0 +1,19 @@
+import os
+
+print("\n=================\n")
+print("calculating the optimal calendar")
+os.system("python optimal_calendar.py")
+
+print("\n=================\n")
+print("calculating the calendar with 1 hub")
+os.system("python script.py 1")
+
+print("\n=================\n")
+print("calculating the calendar with 2 hubs")
+os.system("python script.py 2")
+
+print("\n=================\n")
+print("calculating the calendar with 3 hubs")
+os.system("python script.py 3")
+
+print("\n=================\n")
